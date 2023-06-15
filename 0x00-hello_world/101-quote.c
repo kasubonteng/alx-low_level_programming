1c990f55acb714e6136612028dc6b817fe6e8a73
Kwadwo Asubonteng <kasubonteng2002@gmail.com>
2023-06-15 23:27:07 +0000

    fixed betty standards

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
- *		  followed by a new line, to the standard error.
+ * main - prints exactly and that piece of art is useful" - Dora Korpar,
+ *			2015-10-19, followed by a new line, to the standard error.
  *
  * Return: 1
   */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf
+	(
+		stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
 
 	return (1);
 }

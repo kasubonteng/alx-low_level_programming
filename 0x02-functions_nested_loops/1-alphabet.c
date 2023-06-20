33826a3d85882e3425db8dc3afe988ca2de3a882
Kwadwo Asubonteng <kasubonteng2002@gmail.com>
2023-06-20 19:48:30 +0000

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets from a-z
+ *
+ * Return: void
+  */
+
+void print_alphabet(void)
+{
+
+	char c = 'a';
+
+	while (c != 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
